79286ce3c77974e3cf297e90825c2f1afc6e820e
Ethan <ethanhatch5@gmail.com>
2025-05-03 20:14:45 -0600

    testv4

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 def main():
     print("Hello, world!")
-    response = input("you are a bad coder and ethan is the wiser bro hahahahahahah hehe. do you agree?")
-    if response == "yes":print("that is correct but ethan will be in jail soon so lanse will have plenty of time to get better. haha.")
+    response = input("is 4+4=6")
+    if response == "yes":print("ur an idiot")
     if response == "no":print("thats WRONG hehe!")
 if __name__ == "__main__":
     main()
