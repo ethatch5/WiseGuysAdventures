6dfa384615e3646f34826f3d8e193c68595d5c24
Lanse Larsen <lolars60@icloud.com>
2025-05-03 21:12:04 -0500

    Merge remote-tracking branch 'origin/main'

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 def main():
     print("Hello, world!")
     response = input("you are a bad coder and ethan is the wiser bro hahahahahahah hehe. do you agree?")
-    if response == "yes":print("Correct for now but lanse will be better soon")
+    if response == "yes":print("that is correct but ethan will be in jail soon so lanse will have plenty of time to get better. haha.")
     if response == "no":print("thats WRONG hehe!")
 if __name__ == "__main__":
     main()
