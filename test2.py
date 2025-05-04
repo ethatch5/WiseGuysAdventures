68ada2caf80703d259039829bb3a4928788cdc8e
Ethan <ethanhatch5@gmail.com>
2025-05-03 20:19:41 -0600

    testv6

--- test2.py
+++
@@ -1,8 +0,0 @@
-def test():
-    print("goodbye world")
-    dookie = input("do you like stuff")
-    if dookie == "yes":print("he likes stuff")
-    if dookie == "no":print("he doesnt like stuff")
-
-if __name__ == "__main__":
-    test()
