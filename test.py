29fda57aca31a2707a9f38c30ab230366be593fc
Ethan <ethanhatch5@gmail.com>
2025-05-03 19:44:50 -0600

    testv2

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 def main():
     print("Hello, world!")
-
+    response = input("you are a bad coder and ethan is the wiser bro hahahahahahah hehe. do you agree?")
+    if response == "yes":print("thats correct!")
+    if response == "no":print("thats WRONG hehe!")
 if __name__ == "__main__":
     main()
